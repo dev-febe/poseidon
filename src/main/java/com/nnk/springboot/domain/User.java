eb2917a79d5194058a55346642e57d6c0e90fb23
kone <ben.kone@akiltechnologies.com>
2021-04-15 16:20:33 +0000

    Story 6: Develop scaffolding code for managing User Entity

--- src/main/java/com/nnk/springboot/domain/User.java
+++ src/main/java/com/nnk/springboot/domain/User.java
@@ -2,21 +2,34 @@ package com.nnk.springboot.domain;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    @NotBlank(message = "Username is mandatory")
+    @NotBlank(message = "username is mandatory")
     private String username;
-    @NotBlank(message = "Password is mandatory")
+    //pattern to validate the password
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+            message = "the password should contain 8 characters and should have letter in uppercase or lowercase")
     private String password;
-    @NotBlank(message = "FullName is mandatory")
+    @NotBlank(message = "fullName is mandatory")
     private String fullname;
-    @NotBlank(message = "Role is mandatory")
+    @NotBlank(message = "role is mandatory")
     private String role;
+
+    public User(String username, String password, String fullname, String role) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.role = role;
+    }
+
+    public User() {
+    }
 
     public Integer getId() {
         return id;
